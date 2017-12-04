f69b3d9cb693a37736e72483656d53d0571eaed8
unknown <850002098@qq.com>
2017-12-04 23:45:12 +0800

    update

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -9,55 +9,41 @@ def compute_accuracy(v_x, v_y):
     result = sess.run(accuracy,feed_dict={X: v_x, Y: v_y, keep_prob:1})
     return result
 
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-    
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-def conv2d(x, W):
-    # strides=[1,x_movement,y_movement,1]
+def getConv(x, W):
     return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
 
 if __name__ == '__main__':
-    # load mnist data
     mnist = input_data.read_data_sets("./Mnist_data/", one_hot=True)
 
     X = tf.placeholder(tf.float32, [None, 784])
     Y = tf.placeholder(tf.float32, [None, 10])
     keep_prob = tf.placeholder(tf.float32)
-    # reshape(data you want to reshape, [-1, reshape_height, reshape_weight, imagine layers]) image layers=1 when the imagine is in white and black, =3 when the imagine is RGB
     x_image = tf.reshape(X, [-1, 28, 28, 1])
 
     # ********************** conv1 *********************************
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)  # output size 28*28*32
-    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    # output size 14*14*32
+    W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
+    b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]))
+    h_conv1 = tf.nn.relu(getConv(x_image, W_conv1) + b_conv1)  # output size 28*28*32
+    h_pool1 = tf.nn.max_pool(h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') # output size 14*14*32
 
     # ********************** conv2 *********************************
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)  # output size 14*14*64
-    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    # output size 7*7*64
+    W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+    b_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+    h_conv2 = tf.nn.relu(getConv(h_pool1, W_conv2) + b_conv2)  # output size 14*14*64
+    h_pool2 = tf.nn.max_pool(h_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') # output size 7*7*64
 
     # ********************* func1 layer *********************************
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
+    W_fc1 = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024], stddev=0.1))
+    b_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     # ********************* func2 layer *********************************
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
+    W_fc2 = tf.Variable(tf.truncated_normal([1024, 10], stddev=0.1))
+    b_fc2 = tf.Variable(tf.constant(0.1, shape=[10]))
     prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
-    # calculate the loss
     cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(prediction), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
--- performRecognition.py
+++ performRecognition.py
@@ -62,8 +62,8 @@ def restore_cnn():
     return sess, prediction, X, keep_prob
 
 def getResult_cnn(roi, sess, prediction, X, keep_prob):
-    nbr = sess.run(prediction, feed_dict={X: [roi], keep_prob:1})
-    return nbr
+    labelPre = sess.run(prediction, feed_dict={X: [roi], keep_prob:1})
+    return labelPre
 
 if __name__ == '__main__':
 
@@ -75,63 +75,60 @@ if __name__ == '__main__':
     args = {}
     args["classiferPath"] = './digits_cls.pkl'
     args["image"] = './photo_cdr2.jpg'
-
-    # Read the input image
-    im = cv2.imread(args["image"])
+    image = cv2.imread(args["image"])
 
     # Convert to grayscale and apply Gaussian filtering
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image_gray = cv2.GaussianBlur(image_gray, (5, 5), 0)
 
     # Threshold the image
     # cv2.threshold(src,thresh,maxval,type[,dst])->retval,dst
     # thresh:阈值，maxval:在二元阈值THRESH_BINARY和逆二元阈值THRESH_BINARY_INV中使用的最大值
     # 返回值retval其实就是阈值 type:使用的阈值类型
     # ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV) # photo_1 and photo_2
-    ret, im_th = cv2.threshold(im_gray, 124, 255, cv2.THRESH_BINARY_INV) # photo_cdr1 and photo_cdr2
+    _, image_threshold = cv2.threshold(image_gray, 124, 255, cv2.THRESH_BINARY_INV) # photo_cdr1 and photo_cdr2
     # ret, im_th = cv2.threshold(im_gray, 110, 255, cv2.THRESH_BINARY_INV) # photo_3 and photo_4
 
     # Find contours in the image
-    # 第二个参数表示轮廓的检索模式
-    # cv2.RETR_EXTERNAL表示只检测外轮廓
-    # 第三个参数method为轮廓的近似办法
-    # cv2.CHAIN_APPROX_SIMPLE压缩水平方向，垂直方向，对角线方向的元素，只保留该方向的终点坐标
+    # 第二个参数表示轮廓的检索模式，cv2.RETR_EXTERNAL表示只检测外轮廓
+    # 第三个参数method为轮廓的近似办法，cv2.CHAIN_APPROX_SIMPLE压缩水平方向，垂直方向，对角线方向的元素，只保留该方向的终点坐标
     # 返回三个值，一个是图像本身，一个是轮廓集合，还有一个是每条轮廓对应的属性
-    _, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, __ = cv2.findContours(image_threshold.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Get rectangles contains each contour
-    # 得到包含轮廓的矩形，返回值是两个顶点
-    rects = [cv2.boundingRect(ctr) for ctr in ctrs]
+    # 得到包含轮廓的矩形，返回值是顶点和宽高
+    rectangles = [cv2.boundingRect(contour) for contour in contours]
 
     # Get sigmoid or cnn parameters
     # sigmoid_parameters = restore_sigmiod()
     cnn_parameters = restore_cnn()
 
     # For each rectangular region, calculate HOG features and predict
-    for rect in rects:
-        # Draw the rectangles | cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 3)
-        cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 133, 133), 1)
-        # Make the rectangular region around the digit
-        leng = int(rect[3] * 1.6)
-        pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-        pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-        roi = im_th[pt1:pt1 + leng, pt2:pt2 + leng]
+    for rect in rectangles:
+        # Draw the rectangles | cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        x = rect[0]; y = rect[1]; w = rect[2]; h = rect[3]
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 133, 133), 1)
+        # Make the rectangular region around the digit, get region of interest(ROI)
+        new_h = int(h * 1.618)
+        new_y = int(y + h // 2 - new_h // 2)
+        new_x = int(x + w // 2 - new_h // 2)
+        ROI = image_threshold[new_y:new_y + new_h, new_x:new_x + new_h]
         # Resize the image
         try:
-            roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+            ROI = cv2.resize(ROI, (28, 28), interpolation=cv2.INTER_AREA) # 区域插值
         except:
             print("Wrong contour!")
-            continue
-        roi = cv2.dilate(roi, (3, 3))
-        roi = roi.flatten()
-        roi = roi / 255
+            exit(-1)
+        ROI = cv2.dilate(ROI, (3, 3)) # 膨胀
+        ROI = ROI.flatten() # 变平
+        ROI = ROI / 255
 
         # Calculate the HOG features
-        nbr = getResult_cnn(roi, cnn_parameters[0], cnn_parameters[1], cnn_parameters[2], cnn_parameters[3])
+        labelPre = getResult_cnn(ROI, cnn_parameters[0], cnn_parameters[1], cnn_parameters[2], cnn_parameters[3])
         # nbr = getResult_sigmoid(roi, sigmoid_parameters[0], sigmoid_parameters[1], sigmoid_parameters[2])
-        print(nbr, end=' ')
-        cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (199, 0, 0), 1)
+        print(labelPre, end=' ')
+        cv2.putText(image, str(int(labelPre[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (199, 0, 0), 1)
 
     cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
-    cv2.imshow("Resulting Image with Rectangular ROIs", im)
+    cv2.imshow("Resulting Image with Rectangular ROIs", image)
     cv2.waitKey()
